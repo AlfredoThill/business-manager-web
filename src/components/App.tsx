92f031ea47f928993d3c639aa0049db1fa896a72
Alfredo <alfredothill@gmail.com>
2022-10-06 00:19:09 -0500

    eslint, prettier and basic router

---
+++ src/components/App.tsx
@@ -0,0 +1,40 @@
+import logo from '../resources/logo.svg'
+import styles from './App.module.css'
+import React, { Suspense, lazy } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// const Home = lazy(() => import('./routes/Home'));
+// const About = lazy(() => import('./routes/About'));
+
+const Home = () => {
+  return (
+    <div className={styles.App}>
+      <header className={styles['App-header']}>
+        <img src={logo} className={styles['App-logo']} alt='logo' />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className={styles['App-link']}
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Suspense>
+  </Router>
+)
+
+export default App
