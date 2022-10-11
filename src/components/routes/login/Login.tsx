88d89dfe7091b1034e64dc9deca35433739a67c5
Alfredo <alfredothill@gmail.com>
2022-10-10 20:49:23 -0500

    sample login

--- src/components/routes/login/Login.tsx
+++ src/components/routes/login/Login.tsx
@@ -1,5 +1,15 @@
-import CustomInput from '../../common/CustomInput';
-import { Button } from '@mui/material';
+import * as React from 'react';
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  Box,
+  Typography,
+  Container
+} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Copyright from '../../common/Copyright';
 
 export interface ILogin {
   handleLogin(): void;
@@ -17,32 +27,59 @@ const Login: React.FC<ILogin> = ({
   error,
 }: ILogin) => {
   return (
-    <div>
-      <form>
-        <CustomInput
-          labelText='Email'
-          id='email'
-          formControlProps={{
-            fullWidth: true,
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
           }}
-          handleChange={handleEmailChange}
-          type='text'
-        />
-        <CustomInput
-          labelText='Password'
-          id='password'
-          formControlProps={{
-            fullWidth: true,
-          }}
-          handleChange={handlePasswordChange}
-          type='password'
-        />
-        <Button type='button' color='primary' onClick={handleLogin}>
-          Log in
-        </Button>
-      </form>
-    </div>
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Log In
+          </Typography>
+          <Box component="form" sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={handleEmailChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handlePasswordChange}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleLogin}
+            >
+              Log In
+            </Button>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
   );
-};
+}
 
 export default Login;
